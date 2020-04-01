b58a94ef54a4289d490a0b64275f5283de84fda1
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-04-01 17:24:09 -0500

    add uploadRouter to handle images uploaded to the server

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 // Use mongoose
 const mongoose = require('mongoose');
@@ -95,6 +96,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
