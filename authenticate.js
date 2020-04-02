6c7cd6e7b32f254f10b1e88723e4c7ca258b1965
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-04-02 14:37:11 -0500

    add Facebook OAuth with passport-facebook-token

--- authenticate.js
+++ authenticate.js
@@ -3,7 +3,8 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); //creates, signs, and verifies tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 
@@ -48,4 +49,35 @@ exports.verifyAdmin = ((req, res, next) => {
     err.status = 403;
     return next(err);
   }
-})
+});
+
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (!err && user) { //no error and user already exists
+          return done(null, user);
+        } else { //no error and no user; create new user
+          user = new User( {username: profile.displayName} );
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false)
+            } else {
+              return done(null, user);
+            }
+          })
+        }
+      })
+    }
+  )
+)
